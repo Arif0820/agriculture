e084ebe7afac6c9c66e4ced077f3f70506f0d184
unknown <arifkhan32587@gmail.com>
2025-05-24 14:19:56 +0530

    first commit

---
+++ assets/js/home.js
@@ -0,0 +1,33 @@
+function Cicon() {
+  mobilemenu = document.querySelector('.navs');
+  mobilemenu.classList.toggle('show');
+
+  changicon = document.querySelector('.bar-icon i');
+  changicon.classList.toggle('fa-x');
+}
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    20: {
+      slidesPerView: 1,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+    1110: {
+      slidesPerView: 3,
+      
+    }
+  }
+});
